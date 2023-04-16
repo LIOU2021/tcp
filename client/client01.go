820c9062ee995ef631d6a6becafcb5fbcf462bf8
LIOU2021 <aa78789898tw@gmail.com>
2023-04-16 15:09:40 +0800

    fix

--- client/client01.go
+++ client/client01.go
@@ -7,23 +7,30 @@ import (
 )
 
 func main() {
+	fmt.Println("client start")
+	defer fmt.Println("client end")
 	for i := 0; i < 3; i++ {
 		conn, err := net.Dial("tcp", "localhost:8000")
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		msg := "I'm a Kungfu Dev"
+		msg := "I'm a Dev"
 		if _, err := conn.Write([]byte(msg)); err != nil {
 			log.Fatal(err)
 		}
 
-		buf := make([]byte, len(msg))
+		// buf := make([]byte, len(msg))
+		buf := make([]byte, 1000)
+
 		n, err := conn.Read(buf)
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		fmt.Println(string(buf[:n]))
+		// fmt.Println(string(buf[:n]))
+
+		_ = n
+		fmt.Println(string(buf))
 	}
 }
